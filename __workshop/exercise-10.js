83b800359c8931ff272960e1edf23c86cba6aa24
chris shino <chris.shino@hotmail.com>
2020-09-19 15:44:36 -0400

    question 10 done

--- __workshop/exercise-10.js
+++ __workshop/exercise-10.js
@@ -13,6 +13,17 @@ function exercise10() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+
+  for (let i = 0; i <= 6; i++){
+    let str = ''
+    for (let j = 0; j <= i; j++){
+      str += '#'
+    }
+    console.log(str)
+  }
+
+
+
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
